46856d4b321764d90a8e4e290672dc767aecdd48
SergeyPostnikov <60840361+SergeyPostnikov@users.noreply.github.com>
2022-08-23 20:29:04 +0300

    passcard_info_view implemented

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,19 +1,21 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from .helpers import get_duration, format_duration, is_visit_long
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
+    passcard = Passcard.objects.get(passcode=passcode)
+    this_passcard_visits = []
 
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
+    for visit in Visit.objects.filter(passcard=passcard):
+        data = {
+            'entered_at': visit.entered_at,
+            'duration': format_duration(get_duration(visit)),
+            'is_strange': is_visit_long(visit)
+        }
+        this_passcard_visits.append(data)
+
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
